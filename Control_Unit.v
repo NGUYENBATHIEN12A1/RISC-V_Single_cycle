725fb28a21ecc40618d1eb16fee01571c5408936
MoKuBa <166572750+NGUYENBATHIEN12A1@users.noreply.github.com>
2026-05-08 09:58:07 +0700

    Update Control_Unit.v

--- Control_Unit.v
+++ Control_Unit.v
@@ -1,23 +1,17 @@
 //Control_Unit
-module Control_Unit(instruction, Branch, MemRead, MemtoReg, ALUOp, MemWrite, ALUSrc, RegWrite);
-
-input [6:0] instruction;
-output reg Branch, MemRead, MemtoReg, MemWrite, ALUSrc, RegWrite;
-output reg [1:0] ALUOp;
-
-always @(*) begin
-    case(instruction)
-
-        // R-type
-        7'b0110011 : {ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, ALUOp} <= 8'b00100010;
-        // Load (lw)
-        7'b0000011 : {ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, ALUOp} <= 8'b11110000;
-        // Store (sw)
-        7'b0100011 : {ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, ALUOp} <= 8'b10001000;
-        // Branch (beq)
-        7'b1100011 : {ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, ALUOp} <= 8'b00000101;
-        default    : {ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, ALUOp} <= 8'b00000000;
-    endcase
-end
-
+module Control_Unit(
+    input [6:0] instruction,
+    output reg Branch, MemRead, MemtoReg, MemWrite, ALUSrc, RegWrite,
+    output reg [1:0] ALUOp
+);
+    always @(*) begin
+        case(instruction)
+            7'b0110011 : {ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, ALUOp} = 8'b00100010; // R-type
+            7'b0000011 : {ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, ALUOp} = 8'b11110000; // Load
+            7'b0100011 : {ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, ALUOp} = 8'b10001000; // Store
+            7'b1100011 : {ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, ALUOp} = 8'b00000101; // Branch
+            7'b0010011 : {ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, ALUOp} = 8'b10100000; // I-type (addi)
+            default    : {ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, ALUOp} = 8'b00000000;
+        endcase
+    end
 endmodule
