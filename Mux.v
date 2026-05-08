5a6929fd3e79b15831b8b82876e4b86fa25b9203
MoKuBa <166572750+NGUYENBATHIEN12A1@users.noreply.github.com>
2026-05-08 10:00:10 +0700

    Update Mux.v

--- Mux.v
+++ Mux.v
@@ -1,24 +1,7 @@
-//Mutiplexer
-module mux1(
-    input sell1,
-    input [31:0] A1,B1,
-    output [31:0] mux1_out
+module Mux2to1 (
+    input [31:0] in0, in1,
+    input sel,
+    output [31:0] out
 );
-assign mux1_out = (sell1==1'b0) ? A1 :B1;
-endmodule
-
-module mux2(
-    input sell2,
-    input [31:0] A2,B2,
-    output [31:0] mux2_out
-);
-assign mux2_out = (sell2==1'b0) ? A2 :B2;
-endmodule
-
-module mux3(
-    input sell3,
-    input [31:0] A3,B3,
-    output [31:0] mux3_out
-);
-assign mux3_out = (sell3==1'b0) ? A3 :B3;
+    assign out = (sel == 1'b0) ? in0 : in1;
 endmodule
